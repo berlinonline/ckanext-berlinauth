e5ffd02a225b452abbe69da90ac4a5df634e7643
Knud Möller <dunken69@gmail.com>
2022-05-06 16:52:24 +0200

    create less output

--- run_single_test.sh
+++ run_single_test.sh
@@ -4,4 +4,5 @@ export CKAN_INI="/usr/lib/ckan/default/src/ckan/test-core.ini"
 
 # delete .pyc-files to prevent the "import file mismatch" errors
 find -name "*.pyc" -delete
-coverage run --source=ckanext.berlinauth -m pytest -vv --log-cli-level=10 ckanext/berlinauth/tests -k "$1" && coverage html
+# coverage run --source=ckanext.berlinauth -m pytest -vv --log-cli-level=10 ckanext/berlinauth/tests -k "$1" && coverage html
+coverage run --source=ckanext.berlinauth -m pytest ckanext/berlinauth/tests -k "$1" && coverage html
