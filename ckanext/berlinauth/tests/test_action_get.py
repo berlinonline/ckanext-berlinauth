e80af0bad1869c6377535a2e6d657bc311fb955f
Knud Möller <dunken69@gmail.com>
2022-10-13 17:56:22 +0200

    move fixtures to module level (ctd)

--- ckanext/berlinauth/tests/test_action_get.py
+++ ckanext/berlinauth/tests/test_action_get.py
@@ -13,52 +13,6 @@ flask_app = Flask(__name__)
 PLUGIN_NAME = 'berlinauth'
 LOG = logging.getLogger(__name__)
 
-@pytest.fixture
-def sysadmin():
-    '''Fixture to create a sysadmin user.'''
-    return factories.Sysadmin(name='theadmin')
-
-@pytest.fixture
-def org_with_users(app, sysadmin):
-    '''Fixture to create an organization with different kinds of users.'''
-    users = {
-        'admin': factories.User(name='org_admin'),
-        'editor': factories.User(name='org_editor'),
-    }
-    org = factories.Organization(name='theorg')
-    group = factories.Group(name='thegroup')
-
-    for role, user in users.items():
-        member = {
-            'username': user['name'],
-            'role': role,
-            'id': org['name'],
-        }
-        app.post(
-            url='/api/3/action/organization_member_create',
-            json=member,
-            extra_environ={'Authorization': sysadmin['apikey']},
-        )
-    
-        member = {
-            'username': user['name'],
-            'role': role,
-            'id': group['name'],
-        }
-        app.post(
-            url='/api/3/action/group_member_create',
-            json=member,
-            extra_environ={'Authorization': sysadmin['apikey']},
-        )
-    
-    return {
-        'organization': org,
-        'group': group,
-        'users': users,
-        'sysadmin': sysadmin
-    }
-
-
 @pytest.mark.ckan_config('ckan.plugins', f'{PLUGIN_NAME}')
 @pytest.mark.usefixtures('clean_db', 'clean_index', 'with_plugins')
 class TestOrganizationList(object):
